082f9496398c8e4be5abced4370a06cc0f182563
antonholtback <anton.holtback@outlook.com>
2023-09-13 12:50:18 +0200

    pill

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+console.log('lol');
+/*
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
+    document.querySelector('.header').style.top = "0";
     console.log('lmao');
   } else {
-    document.getElementById("header").style.top = "-150px";
+    document.querySelector('.header').style.top = "-150px";
   }
   prevScrollpos = currentScrollPos;
 }
+*/
